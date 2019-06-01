b6111be80ea559378ea00ced4f80c38251c14233
Matthew Wegener <mwegener@protonmail.com>
2019-06-01 15:15:31 -0700

    Mul name change + reg initial code

--- Assignment 1/mul.h
+++ Assignment 1/mul.h
@@ -1,14 +1,14 @@
 #include "systemc.h"
 
-SC_MODULE(mul_cst){
+SC_MODULE(mul){
   sc_in<float> A,CST;
   sc_out<float> M;
 
-  void do_mul_cst(){
+  void do_mul(){
     M.write(A.read() * CST);
   }
 
-  SC_CTOR(mul_cst){
+  SC_CTOR(mul){
     SC_METHOD(do_mul_cst);
     sensitive << A;
   }
---
+++ Assignment 1/reg.h
@@ -0,0 +1,25 @@
+#include "systemc.h"
+
+SC_MODULE(reg){
+  sc_in<float> in
+  sc_out<float> out;
+  sc_in_clk clock;
+  sc_in<bool> reset;
+
+  //z delay register
+  void do_reg(){
+    while(1){
+      if(reset.read() == true){
+        out.write(0);
+      }
+      else
+      {
+        out.write(in.read());
+      }
+      wait();
+    }
+  }
+  SC_CTOR(reg){
+    SC_CTHREAD(do_reg,clock.pos());
+  }
+};
