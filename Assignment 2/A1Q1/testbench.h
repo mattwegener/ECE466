a14f5c2b9d9bff08387f46f8a602e58d74c06b94
Matthew Wegener <mwegener@protonmail.com>
2019-06-19 16:16:13 -0700

    [A2Q1] Read Byte Test

--- Assignment 2/A1Q1/testbench.h
+++ Assignment 2/A1Q1/testbench.h
@@ -17,22 +17,49 @@ SC_MODULE(testbench){
   sc_in <bool> complete; // Output port signaling “done”
   sc_in_clk clk;
   sc_out <bool> reset;
-  
+
   void testbench1(){
     // Temp variables for comparison
     sc_lv <8> data_read;
 
     // Reset system at start.
-    reset.write(true); 
+    reset.write(true);
     wait();
     reset.write(false);
     wait();
-    
+
     while(1){
-      
+
+      //Read from byte and check data bus release
+      cout << "Test 1: Read from byte" << endl;
+      cout << "Expectations: data = XXXXXXXX" << endl;
+      addr.write(0);
+      comm.write(RDBYT);
+      wait();
+      new_comm.write(true);
+      while(!complete.read()){
+        wait();
+      }
+      data_read = data.read();
+      cout << "@" << sc_time_stamp() << ": RDBYT, address = " << addr << ", data = " << data_read << endl;
+      cout << endl;
+      new_comm.write(false);
+      wait();
+      wait();
+      cout << "Test 2: Checking Data bus is properely released" <<endl;
+      cout << "Expectations: Data = ZZZZZZZZ" <<endl;
+      data_read = data.read();
+      cout << "@" << sc_time_stamp() << "Data Release Check, data = " << data_read << endl;
+      cout << endl;
+
+      //Simulation end
+      cout << "Tests Complete" << endl;
+      sc_stop();
+      wait();
+
     }
   }
-  
+
   SC_CTOR(testbench){
     SC_CTHREAD(testbench1, clk.pos());
   }
