50df1b4a2da8ec9aada5649c51f828971583e98c
Matthew Wegener <mwegener@protonmail.com>
2019-06-20 00:04:20 -0700

    Fifo main declaration fix

--- Assignment 2/A2Q2/main.cpp
+++ Assignment 2/A2Q2/main.cpp
@@ -10,7 +10,7 @@ int sc_main(int argc, char* argv[]){
 	sc_clock ClkFast ("ClkFast", 50,  SC_NS, 0.5, 1, SC_NS);
 	sc_clock ClkSlow ("ClkSlow", 100, SC_NS, 0.5, 1, SC_NS);
 
-  fifo<int,8> Fifo1 (”F1”, 8); // 8-character FIFO
+  fifo<int,8> Fifo1 (”F1”); // 8-character FIFO
   producer<int> P1 ("P1"); P1.out (Fifo1); P1.Clock (ClkFast);
   consumer<int> C1 ("C1"); C1.in (Fifo1); C1.Clock (ClkSlow);
 
